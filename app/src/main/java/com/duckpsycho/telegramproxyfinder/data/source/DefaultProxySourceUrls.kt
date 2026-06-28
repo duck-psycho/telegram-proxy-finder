614268bb7bc1ef2872d7dadba3dcef2f687b554a
Duckpsycho <duckpsycho.developer@gmail.com>
2026-06-29 00:39:24 +0300

    chore: update default proxy source urls

--- app/src/main/java/com/duckpsycho/telegramproxyfinder/data/source/DefaultProxySourceUrls.kt
+++ app/src/main/java/com/duckpsycho/telegramproxyfinder/data/source/DefaultProxySourceUrls.kt
@@ -3,10 +3,7 @@ package com.duckpsycho.telegramproxyfinder.data.source
 object DefaultProxySourceUrls {
     val urls = listOf(
         "https://raw.githubusercontent.com/sakha1370/V2rayCollector/refs/heads/main/active_mtproto_proxies.txt",
-        "https://raw.githubusercontent.com/SoliSpirit/mtproto/refs/heads/master/all_proxies.txt",
-        "https://raw.githubusercontent.com/Surfboardv2ray/TGProto/refs/heads/main/proxies-tested.txt",
-        "https://raw.githubusercontent.com/Argh94/Proxy-List/refs/heads/main/MTProto.txt",
-        "https://raw.githubusercontent.com/kort0881/telegram-proxy-collector/refs/heads/main/proxy_list.txt",
+        "https://raw.githubusercontent.com/kort0881/telegram-proxy-collector/refs/heads/main/proxy_all.txt",
         "https://raw.githubusercontent.com/devho3ein/tg-proxy/refs/heads/main/proxys/All_Proxys.txt",
         "https://raw.githubusercontent.com/Grim1313/mtproto-for-telegram/refs/heads/master/all_proxies.txt",
     )
