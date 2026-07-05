1bcc6f6b9c4129f94fa94d7345390e7fed9802d2
Duckpsycho <duckpsycho.developer@gmail.com>
2026-07-05 14:09:19 +0300

    chore: bump version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.duckpsycho.telegramproxyfinder"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         ndk {
             abiFilters += "arm64-v8a"
